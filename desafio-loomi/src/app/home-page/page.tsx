2f9fe42872f7cddf1fbdf43634a2b02502fa3cf1
othonaf <othon.ab@hotmail.com>
2024-12-17 11:28:39 -0300

    Personalize o navbar aplicando comportamento de mouse hover.

--- desafio-loomi/src/app/home-page/page.tsx
+++ desafio-loomi/src/app/home-page/page.tsx
@@ -8,8 +8,10 @@ import { useEffect, useState } from "react";
 import SideBar from "@/components/SideBar";
 
 export default function HomePage() {
-  const [ticketData, setTicketData] = useState({ value: 0, growth: 0 });
+  const [ticketDataDaily, setticketDataDaily] = useState({ value: 0, growth: 0 });
+  const [ticketDataMonthly, setticketDataMonthly] = useState({ value: 0, growth: 0 });
 
+  //Código para receber dados Diários:
   const dadosTicketDay = async () => {
     const url = "https://628bf017667aea3a3e387e51.mockapi.io/avg-ticket-day";
     try {
@@ -22,7 +24,7 @@ export default function HomePage() {
         return;
 
       }
-      setTicketData(dailyData);
+      setticketDataDaily(dailyData);
     } catch (error) {
       console.log(`Erro ao tentar acessar dados diários: ${error}`)
     }
@@ -31,57 +33,93 @@ export default function HomePage() {
     dadosTicketDay();
   }, []);
 
-  const formattedValue = new Intl.NumberFormat('pt-BR', {
+  //Código para receber dados Mensais:
+  const dadosTicketMonth = async () => {
+    const url = "https://628bf017667aea3a3e387e51.mockapi.io/avg-ticket-month";
+    try {
+      const response = await axios.get(url);
+      console.log(response.data);
+      const monthlyData = response.data;
+
+      if (!monthlyData || monthlyData == null) {
+        console.log("dados diários vazio.")
+        return;
+
+      }
+      setticketDataMonthly(monthlyData);
+    } catch (error) {
+      console.log(`Erro ao tentar acessar dados diários: ${error}`)
+    }
+  }
+  useEffect(() => {
+    dadosTicketMonth();
+  }, []);
+
+  //Bloco de código para formatar o valor em R$:
+  const formattedDailyValue = new Intl.NumberFormat('pt-BR', {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2
-  }).format(ticketData.value);
+  }).format(ticketDataDaily.value);
+
+  const formattedMonthlyValue = new Intl.NumberFormat('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+  }).format(ticketDataMonthly.value);
 
   return (
-    <div
-      className="min-h-screen flex flex-col items-center justify-center bg-gray-50 pt-20 py-12 px-4 sm:px-6 lg:px-8 relative"
-      style={{ backgroundImage: `url(${backg.src})`, backgroundSize: "cover" }}
-    >
-      <Navbar />
+    <div className="min-h-screen flex bg-gray-50 relative">
       <SideBar />
-      <div className="container mx-auto p-4">
-        <h1 className="text-2xl font-ubuntu font-bold mb-4 text-black">
-          Início
-        </h1>
+      
+      <div className="flex-1 flex flex-col pl-[108px] pr-6 overflow-x-hidden">
+        <Navbar />
         
-        <div className="flex flex-wrap justify-between space-y-8 sm:space-y-0 sm:space-x-2">
-          <DashCards
-            titulo="Ticket Médio Últimas 24h"
-            subtitulo={`${ticketData.growth > 0 ? '+' : ''} ${ticketData.growth}% em relação ao dia anterior`}
-      valor={formattedValue}
-          />
-          <DashCards
-            titulo="Ticket Médio mensal"
-            subtitulo="+ 15% em relação a julho"
-            valor="129.292,00"
-          />
-          <DashCards
-            titulo="Ticket Médio Últimas 24h"
-            subtitulo="+ 15% em relação ao dia anterior"
-            valor="9.292,00"
-          />
-          <DashCards
-            titulo="Ticket Médio mensal"
-            subtitulo="+ 15% em relação a julho"
-            valor="129.292,00"
-          />
-          <DashCards
-            titulo="Ticket Médio Últimas 24h"
-            subtitulo="+ 15% em relação ao dia anterior"
-            valor="9.292,00"
-          />
-          <DashCards
-            titulo="Ticket Médio mensal"
-            subtitulo="+ 15% em relação a julho"
-            valor="129.292,00"
-          />
-        </div>
-        <Dashboard />
+        <main className="flex-1 p-6 pt-24">
+          <div 
+            className="w-full h-full relative space-x-4"
+            style={{ backgroundImage: `url(${backg.src})`, backgroundSize: "cover" }}
+          >
+            <h1 className="text-2xl font-ubuntu font-bold mb-6 text-black">
+              Início
+            </h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-8 ">
+              <DashCards
+                titulo="Ticket Médio Últimas 24h"
+                subtitulo={`${ticketDataDaily.growth > 0 ? '+' : ''} ${ticketDataDaily.growth}% em relação ao dia anterior`}
+                valor={formattedDailyValue}
+              />
+              <DashCards
+                titulo="Ticket Médio mensal"
+                subtitulo={`${ticketDataMonthly.growth > 0 ? '+' : ''} ${ticketDataMonthly.growth}% em relação ao dia anterior`}
+                valor={formattedMonthlyValue}
+              />
+              <DashCards
+                titulo="Produtos em manutenção"
+                subtitulo="+ 15% em relação ao dia anterior"
+                valor="9.292,00"
+              />
+              <DashCards
+                titulo="Acabando o estoque"
+                subtitulo="+ 15% em relação a julho"
+                valor="129.292,00"
+              />
+              <DashCards
+                titulo="Pedidos realizados no mês"
+                subtitulo="+ 15% em relação ao dia anterior"
+                valor="9.292,00"
+              />
+              <DashCards
+                titulo="Pedidos vendidos no mês"
+                subtitulo="+ 15% em relação a julho"
+                valor="129.292,00"
+              />
+            </div>
+            
+            <Dashboard />
+          </div>
+        </main>
       </div>
     </div>
   );
+  
 }
--- desafio-loomi/src/components/SideBar.tsx
+++ desafio-loomi/src/components/SideBar.tsx
@@ -4,46 +4,62 @@ import person from "@/assets/person.svg";
 import cat from "@/assets/cat.svg";
 import log from "@/assets/log.svg";
 import menu from "@/assets/menu.svg";
-import services from "@/assets/services.svg"
+import services from "@/assets/services.svg";
 import home from "@/assets/home.svg";
+import retroceder from "@/assets/retroceder.svg";
+import card from "@/assets/card.svg";
+import text from "@/assets/text.svg";
+import gear from "@/assets/gear.svg";
+import { useState } from "react";
 
 export default function SideBar() {
+  const [menuIcon, setMenuIccon] = useState(menu)
+
   return (
-    <div className="absolute top-[100px] left-4 w-[88px] shadow-xl h-[964px] bg-white bg-no-repeat bg-[padding-box] shadow-md rounded-lg opacity-100 flex flex-col items-center py-4 space-y-4">
-      {" "}
-      <Image
-        src={menu}
-        alt="Home"
-        width={40}
-        color="black"
-        height={40}
-        className="mb-4"
-      />{" "}
-      <Image src={home} alt="Tools" width={40} height={40} className="mb-4" />{" "}
-      <Image
-        src={cat}
-        alt="Layers"
-        width={40}
-        height={40}
-        className="mb-4"
-      />{" "}
-      <Image src={services} alt="Truck" width={40} height={40} className="mb-4" />{" "}
-      <Image src={log} alt="Buy" width={40} height={40} className="mb-4" />{" "}
-      <Image
-        src={buy}
-        alt="Message"
-        width={40}
-        height={40}
-        className="mb-4"
-      />{" "}
-      <Image src={person} alt="User" width={40} height={40} className="mb-4" />{" "}
-      <Image
-        src={buy}
-        alt="Settings"
-        width={40}
-        height={40}
-        className="mb-4"
-      />{" "}
+    <div className="absolute w-[88px] h-[964px] bg-white top-[100px] left-[-4] shadow-[0px_3px_6px_#00000029] rounded-lg flex flex-col items-center py-5 z-50 transition-[width] duration-300 ease-in-out hover:w-[250px] group"
+    onMouseEnter={() => setMenuIccon(retroceder)}
+    onMouseLeave={() => setMenuIccon(menu)}
+    >
+      <div className="flex items-center w-full text-left px-5 py-2.5 transition-colors duration-300 hover:bg-[#EDA268] hover:border hover:border-[#EDA268] hover:rounded-md hover:cursor-pointer">
+        <Image src={menuIcon} alt="Menu" width={40} height={40} />
+        <span className="hidden group-hover:inline-block ml-2 text-left font-ubuntu text-[22px] leading-[30px] tracking-[0.44px] text-[#4E5D66]">Fechar</span>
+      </div>
+      <div className="flex items-center w-full text-left px-5 py-2.5 transition-colors duration-300 hover:bg-[#EDA268] hover:border hover:border-[#EDA268] hover:rounded-md hover:cursor-pointer">
+        <Image src={home} alt="Home" width={40} height={40} />
+        <span className="hidden group-hover:inline-block ml-2 text-left font-ubuntu text-[22px] leading-[30px] tracking-[0.44px] text-[#4E5D66]">Início</span>
+      </div>
+      <div className="flex items-center w-full text-left px-5 py-2.5 transition-colors duration-300 hover:bg-[#EDA268] hover:border hover:border-[#EDA268] hover:rounded-md hover:cursor-pointer">
+        <Image src={cat} alt="Catálogo" width={40} height={40} />
+        <span className="hidden group-hover:inline-block ml-2 text-left font-ubuntu text-[22px] leading-[30px] tracking-[0.44px] text-[#4E5D66]">Catálogo</span>
+      </div>
+      <div className="flex items-center w-full text-left px-5 py-2.5 transition-colors duration-300 hover:bg-[#EDA268] hover:border hover:border-[#EDA268] hover:rounded-md hover:cursor-pointer">
+        <Image src={services} alt="Serviços" width={40} height={40} />
+        <span className="hidden group-hover:inline-block ml-2 text-left font-ubuntu text-[22px] leading-[30px] tracking-[0.44px] text-[#4E5D66]">Serviços</span>
+      </div>
+      <div className="flex items-center w-full text-left px-5 py-2.5 transition-colors duration-300 hover:bg-[#EDA268] hover:border hover:border-[#EDA268] hover:rounded-md hover:cursor-pointer">
+        <Image src={log} alt="Logs" width={40} height={40} />
+        <span className="hidden group-hover:inline-block ml-2 text-left font-ubuntu text-[22px] leading-[30px] tracking-[0.44px] text-[#4E5D66]">Logística</span>
+      </div>
+      <div className="flex items-center w-full text-left px-5 py-2.5 transition-colors duration-300 hover:bg-[#EDA268] hover:border hover:border-[#EDA268] hover:rounded-md hover:cursor-pointer">
+        <Image src={buy} alt="Compras" width={40} height={40} />
+        <span className="hidden group-hover:inline-block ml-2 text-left font-ubuntu text-[22px] leading-[30px] tracking-[0.44px] text-[#4E5D66]">Pedidos</span>
+      </div>
+      <div className="flex items-center w-full text-left px-5 py-2.5 transition-colors duration-300 hover:bg-[#EDA268] hover:border hover:border-[#EDA268] hover:rounded-md hover:cursor-pointer">
+        <Image src={card} alt="Compras" width={40} height={40} />
+        <span className="hidden group-hover:inline-block ml-2 text-left font-ubuntu text-[22px] leading-[30px] tracking-[0.44px] text-[#4E5D66]">Pagamentos</span>
+      </div>
+      <div className="flex items-center w-full text-left px-5 py-2.5 transition-colors duration-300 hover:bg-[#EDA268] hover:border hover:border-[#EDA268] hover:rounded-md hover:cursor-pointer">
+        <Image src={text} alt="Compras" width={40} height={40} />
+        <span className="hidden group-hover:inline-block ml-2 text-left font-ubuntu text-[22px] leading-[30px] tracking-[0.44px] text-[#4E5D66]">Comunicação</span>
+      </div>
+      <div className="flex items-center w-full text-left px-5 py-2.5 transition-colors duration-300 hover:bg-[#EDA268] hover:border hover:border-[#EDA268] hover:rounded-md hover:cursor-pointer">
+        <Image src={person} alt="Usuário" width={40} height={40} />
+        <span className="hidden group-hover:inline-block ml-2 text-left font-ubuntu text-[22px] leading-[30px] tracking-[0.44px] text-[#4E5D66]">Cliente</span>
+      </div>
+      <div className="flex items-center w-full text-left px-5 py-2.5 transition-colors duration-300 hover:bg-[#EDA268] hover:border hover:border-[#EDA268] hover:rounded-md hover:cursor-pointer">
+        <Image src={gear} alt="Configurações" width={40} height={40} />
+        <span className="hidden group-hover:inline-block ml-2 text-left font-ubuntu text-[22px] leading-[30px] tracking-[0.44px] text-[#4E5D66]">Configurações</span>
+      </div>
     </div>
   );
 }
